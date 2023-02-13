c28a7a7b8b2608af33036f1094a1fdb52dfa407e
Konstantina Liagkou <k.liagkou@atc.gr>
2023-02-13 19:19:37 +0200

    end bert ner class

--- src/models/supervised/bert_ner.py
+++ src/models/supervised/bert_ner.py
@@ -10,7 +10,7 @@ from transformers import DataCollatorForLanguageModeling
 from transformers import TrainingArguments
 from transformers import Trainer
 
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 
 import numpy as np
 
@@ -22,7 +22,14 @@ import math
 
 SEED = 42
 
+import torch
+
 from torch.utils.data import Dataset, DataLoader
+
+from torch import cuda
+
+device = 'cuda' if cuda.is_available() else 'cpu'
+print(device)
 
 
 class dataset(Dataset):
@@ -95,6 +102,8 @@ class NerBERT():
     def __init__(self, checkpoint: str,
                  dataset_path: str = None, df: pd.DataFrame = None,
                  sentence_colname: str = "title",
+                 Train: list = None, Test: list = None,
+                 list_tags: list = ['O', 'PLACE'],
                  model_name: str = 'cl-tohoku/bert-base-japanese',  # 'bert-japanese-finetuned-meisho',
                  tokinizer_name: str = 'cl-tohoku/bert-base-japanese',
                  MAX_LEN: int = 60,
@@ -104,6 +113,10 @@ class NerBERT():
                  LEARNING_RATE: float = 1e-05,
                  MAX_GRAD_NORM: int = 10,
                  *args, **kwargs):
+
+        self.Train = Train
+        self.Test = Test
+
         # Hyperparameters
         self.MAX_LEN = MAX_LEN
         self.TRAIN_BATCH_SIZE = TRAIN_BATCH_SIZE
@@ -119,9 +132,16 @@ class NerBERT():
         extend_logging.meta_info(f"kwargs: {self.updated_values}")
 
         # Pretrain Models Name
-        self.model_name = model_name
-        self.tokinizer_name = tokinizer_name
+        self.labels_to_ids = {k: v for v, k in enumerate(list_tags)}  # {'O': 0, 'PLACE': 1}
+        self.ids_to_labels = {v: k for v, k in enumerate(list_tags)}  # {0: 'O', 1: 'PLACE'}
         self.tokenizer = AutoTokenizer.from_pretrained(tokinizer_name)
+
+        self.model_name = model_name
+        self.model = None
+
+        # Initilize
+        self.training_loader = None
+        self.testing_loader = None
 
     def extract_tags(self, data: list):
         """
@@ -240,17 +260,12 @@ class NerBERT():
 
         return data
 
-    def runner(self):
+    def data_loader(self):
         # From position number start/end tags To tag per token
-        train = self.extract_tags(Train)
-        test = self.extract_tags(Test)
-
-        labels_to_ids = {k: v for v, k in enumerate(['O', 'PLACE'])}  # {'O': 0, 'PLACE': 1}
-        ids_to_labels = {v: k for v, k in enumerate(['O', 'PLACE'])}  # {0: 'O', 1: 'PLACE'}
+        train_dataset = self.extract_tags(self.Train)
+        test_dataset = self.extract_tags(self.Test)
 
         # Tranform to dataset hugging Face
-        train_dataset = train
-        test_dataset = test
         # print("FULL Dataset: {}".format(data.shape))
         print("TRAIN Dataset: {}".format(train_dataset.shape))
         print("TEST Dataset: {}".format(test_dataset.shape))
@@ -278,15 +293,15 @@ class NerBERT():
                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])}
                 """
-        training_set = dataset(train_dataset, self.tokenizer, self.MAX_LEN, labels_to_ids)
-        testing_set = dataset(test_dataset, self.tokenizer, self.MAX_LEN, labels_to_ids)
+        training_set = dataset(train_dataset, self.tokenizer, self.MAX_LEN, self.labels_to_ids)
+        testing_set = dataset(test_dataset, self.tokenizer, self.MAX_LEN, self.labels_to_ids)
 
         # for token, label in zip(self.tokenizer.convert_ids_to_tokens(training_set[0]["input_ids"]),
         #                         training_set[0]["labels"]):
         #     print('{0:10}  {1}'.format(token, label))
         """
-        
-        
+
+
             --> Output print    
             [CLS]       0
             「           0
@@ -322,16 +337,22 @@ class NerBERT():
                        'num_workers': 0
                        }
 
-        training_loader = DataLoader(training_set, **train_params)
-        testing_loader = DataLoader(testing_set, **test_params)
+        self.training_loader = DataLoader(training_set, **train_params)
+        self.testing_loader = DataLoader(testing_set, **test_params)
+
+        return training_set
+
+    def runner(self):
+
+        training_set = self.data_loader()
 
         # Fine tune BERT Models
         # TODO check difference from BertForSequenceClassification
-        model = BertForTokenClassification.from_pretrained(self.model_name,
-                                                           num_labels=len(labels_to_ids),
-                                                           return_dict=False)
+        self.model = BertForTokenClassification.from_pretrained(self.model_name,
+                                                                num_labels=len(self.labels_to_ids),
+                                                                return_dict=False)
         # model = AutoModel.from_pretrained('cl-tohoku/bert-base-japanese', num_labels=3)
-        model.to(device)
+        self.model.to(device)
 
         inputs = training_set[2]
         input_ids = inputs["input_ids"].unsqueeze(0)
@@ -342,36 +363,39 @@ class NerBERT():
         attention_mask = attention_mask.to(device)
         labels = labels.to(device)
 
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = self.model(input_ids, attention_mask=attention_mask, labels=labels)
         initial_loss = outputs[0]
 
         tr_logits = outputs[1]
 
-        optimizer = torch.optim.Adam(params=model.parameters(), lr=self.LEARNING_RATE)
+        optimizer = torch.optim.Adam(params=self.model.parameters(),
+                                     lr=self.LEARNING_RATE)
 
         for epoch in tqdm(range(self.EPOCHS)):
             print(f"\nTraining epoch: {epoch + 1}")
-            self.trainner(epoch)
+            self.trainner(optimizer, epoch)
 
         # evaluation metrics
-        labels, predictions = valid(model, testing_loader)
+        labels, predictions = self.valid(self.model, self.testing_loader)
         print(classification_report(labels, predictions))
 
     # Defining the training function on the 80% of the dataset for tuning the bert model
-    def trainner(self, epoch):
+    def trainner(self, optimizer, epoch):
         tr_loss, tr_accuracy = 0, 0
         nb_tr_examples, nb_tr_steps = 0, 0
         tr_preds, tr_labels = [], []
         # put model in training mode
-        model.train()
+        self.model.train()
 
-        for idx, batch in enumerate(training_loader):
+        for idx, batch in enumerate(self.training_loader):
 
             ids = batch['input_ids'].to(device, dtype=torch.long)
             mask = batch['attention_mask'].to(device, dtype=torch.long)
             labels = batch['labels'].to(device, dtype=torch.long)
 
-            loss, tr_logits = model(input_ids=ids, attention_mask=mask, labels=labels)
+            loss, tr_logits = self.model(input_ids=ids,
+                                         attention_mask=mask,
+                                         labels=labels)
             tr_loss += loss.item()
 
             nb_tr_steps += 1
@@ -383,7 +407,7 @@ class NerBERT():
 
             # compute training accuracy
             flattened_targets = labels.view(-1)  # shape (batch_size * seq_len,)
-            active_logits = tr_logits.view(-1, model.num_labels)  # shape (batch_size * seq_len, num_labels)
+            active_logits = tr_logits.view(-1, self.model.num_labels)  # shape (batch_size * seq_len, num_labels)
             flattened_predictions = torch.argmax(active_logits, axis=1)  # shape (batch_size * seq_len,)
 
             # only compute accuracy at active labels
@@ -401,7 +425,7 @@ class NerBERT():
 
             # gradient clipping
             torch.nn.utils.clip_grad_norm_(
-                parameters=model.parameters(), max_norm=MAX_GRAD_NORM
+                parameters=self.model.parameters(), max_norm=self.MAX_GRAD_NORM
             )
 
             # backward pass
@@ -457,8 +481,8 @@ class NerBERT():
                 tmp_eval_accuracy = accuracy_score(labels.cpu().numpy(), predictions.cpu().numpy())
                 eval_accuracy += tmp_eval_accuracy
 
-        labels = [ids_to_labels[id.item()] for id in eval_labels]
-        predictions = [ids_to_labels[id.item()] for id in eval_preds]
+        labels = [self.ids_to_labels[id.item()] for id in eval_labels]
+        predictions = [self.ids_to_labels[id.item()] for id in eval_preds]
 
         eval_loss = eval_loss / nb_eval_steps
         eval_accuracy = eval_accuracy / nb_eval_steps
