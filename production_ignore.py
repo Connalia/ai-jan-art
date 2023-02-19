ecb59361ace41a2992695cc38eae49b1c0cfae03
Konstantina Liagkou <k.liagkou@atc.gr>
2023-02-19 11:20:52 +0200

    add

---
+++ production_ignore.py
@@ -0,0 +1,15 @@
+# # Default ignored files
+# .idea
+# */__pycache__
+# results/*
+# *.pyc
+#
+# doc
+# doc/*
+#
+# src/preprocessing/time_difference.py
+# tests/preprocessing/test_time_difference.py
+#
+# # Git Ignore Public Repository
+# /Pipfile.lock
+# /notebooks/tutorials/
