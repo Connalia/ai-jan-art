e3c208a026d05c5eedee5e217f0cd56f488ed88a
Konstantina Liagkou <k.liagkou@atc.gr>
2023-02-19 12:13:57 +0200

    add logs format

--- tasks/ner_places/main.py
+++ tasks/ner_places/main.py
@@ -6,24 +6,36 @@ from tasks.ukiyo_loader import UkiyoDataLoader, UkiyoDataOptions
 from tasks.ner_places.gazetteer import ModelGazetteer
 from tasks.ner_places.geolod import ModelGeolod
 
+'''
+Fine-tuning means taking some machine learning model that 
+has already learned something before (i.e. been trained on some data) 
+and then training that model (i.e. training it some more, possibly on different data).
+ 
+ VS
+ 
+Transfer learning means to apply the knowledge that some machine learning model holds 
+(represented by its learned parameters) to a new (but in some way related) task.
+'''
+
 
 class NerModel(StrEnum):
     GAZETTEER = auto()
     GEOLOD = auto()
-    BERT_FINETUNE = auto()
+    BERT_NER = auto()
+    FINE_TUNE_BERT_NER = auto()
     SPACY = auto()
 
 
-def main() -> None:
-
-    model_run = [NerModel.GAZETTEER, NerModel.GEOLOD]
-
+def main(models: list) -> None:
     df_test = UkiyoDataLoader(type_of_dataset=UkiyoDataOptions.TEST_TITLE).loader()
 
-    if NerModel.GAZETTEER in model_run:
+    if NerModel.GAZETTEER in models:
         ModelGazetteer(df_test).runner()
-    if NerModel.GEOLOD in model_run:
+    if NerModel.GEOLOD in models:
         ModelGeolod(df_test).runner()
 
+
 if __name__ == "__main__":
-    main()
+    model_run = [NerModel.GAZETTEER,
+                 NerModel.GEOLOD]
+    main(models=model_run)
